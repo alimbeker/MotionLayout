cbb3f3671724195e66956b705a84dafdc8b57f8f
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-11-04 18:19:50 +0600

    add viewbinding

---
+++ app/src/main/java/com/example/motionlayout/CardsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.motionlayout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.motionlayout.databinding.FragmentCardsBinding
+import com.example.motionlayout.databinding.FragmentStartBinding
+
+class CardsFragment : Fragment() {
+    private lateinit var binding : FragmentCardsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCardsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/example/motionlayout/StartFragment.kt
@@ -0,0 +1,23 @@
+package com.example.motionlayout
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.motionlayout.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+    private lateinit var binding : FragmentStartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
